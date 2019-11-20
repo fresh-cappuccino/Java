8bc8276a860a3875e78fdcfaad86cdfa32895c8b
fresh-cappuccino <57273650+fresh-cappuccino@users.noreply.github.com>
2019-11-20 18:24:55 -0300

    UPDT

--- alunosJSF/src/main/java/br/com/alunosJSF/JasperPdfGenerator.java
+++ alunosJSF/src/main/java/br/com/alunosJSF/JasperPdfGenerator.java
@@ -15,7 +15,7 @@ import net.sf.jasperreports.engine.JasperRunManager;
 
 public class JasperPdfGenerator {
 	private Connection getConexao() throws SQLException, ClassNotFoundException {
-		Connection conexao = DriverManager.getConnection("jdbc:oracle:thin:@192.168.20.57:1521:DESENV", "leoferreira", "1234-leo");
+		Connection conexao = DriverManager.getConnection("jdbc:oracle:thin:@192.168.20.57:1521:DESENV", "leoandrade", "password");
 		return conexao;
 	}
 	
