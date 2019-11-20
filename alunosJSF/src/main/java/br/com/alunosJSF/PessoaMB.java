786c3fce361a993a0aed44ae81ce06204b98e8e6
fresh-cappuccino <57273650+fresh-cappuccino@users.noreply.github.com>
2019-11-20 18:29:27 -0300

    UPDT

--- alunosJSF/src/main/java/br/com/alunosJSF/PessoaMB.java
+++ alunosJSF/src/main/java/br/com/alunosJSF/PessoaMB.java
@@ -459,7 +459,7 @@ public class PessoaMB implements Serializable {
 		this.pdf = pdf;
 	}
 	
-	private Connection getConexao() throws SQLException, ClassNotFoundException {
+	/*private Connection getConexao() throws SQLException, ClassNotFoundException {
 		Connection conexao = DriverManager.getConnection("jdbc:oracle:thin:@192.168.20.57:1521:DESENV", "leoferreira", "1234-leo");
 		return conexao;
 	}
@@ -543,7 +543,7 @@ public class PessoaMB implements Serializable {
 		
 //		System.out.println(stream.available());
 //		return file;
-	}
+	}*/
 	
 	public void addPessoa() {
 		if (validador.login(login.getUsuario(), login.getConfirmaUsuario(), login.getConfirmaSenha(), login.getDefaultPass())) {
